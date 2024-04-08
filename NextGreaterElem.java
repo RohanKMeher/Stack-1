1adedf82930562f821756b0f1e3ac8a124fcaace
RohanKMeher <roha10meher@gmail.com>
2024-04-07 22:46:29 -0400

    JAVA: Stack-1  completed: RKM

---
+++ NextGreaterElem.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+
+    //Tc: O(n)
+    //Sc: O(n)
+    public int[] nextGreaterElements(int[] nums) {
+        
+        Stack<Integer> stack=new Stack<>();
+        for(int i=nums.length-1;i>=0;i--){
+            stack.push(nums[i]);
+        }
+        
+        for(int i=nums.length-1;i>=0;i--){
+            int number=nums[i];
+                while(!stack.isEmpty() && stack.peek()<=nums[i]){
+                    stack.pop();
+                }
+           
+            nums[i]=stack.empty()?-1:stack.peek();
+            stack.push(number);
+        }
+        
+        return nums;
+    }
+}
